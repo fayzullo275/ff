701c79154237b2c9a77e500571dc8ea66dbd43de
ff <ff>
2022-11-23 16:44:22 +0300

    ff

---
+++ script.js
@@ -0,0 +1,10 @@
+var age = +prompt ('Yoshingizni kiriting')
+if (age  > 55 && age <= 150) {
+    alert("Yaqinda pensiyaga chiqasiz")
+}else if(age > 18 && age <=54){
+    alert("Siz hali o'qishingiz kerak")
+}else if(age > 1 && age <= 17){
+    alert("Siz hali yoshsiz")
+}else{
+    alert("Sizda nmadir xato")
+}
